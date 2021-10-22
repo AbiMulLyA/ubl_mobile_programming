6817c59600406af1e617942b7826171e6d6dd475
Ahmad Abi Mulya <>
2021-10-22 20:23:06 +0700

    fix layout calaculator

--- app/src/main/java/com/example/tugasmobileprog/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/tugasmobileprog/ui/home/HomeFragment.java
@@ -1,29 +1,25 @@
 package com.example.tugasmobileprog.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
 import com.example.tugasmobileprog.R;
-import com.maltaisn.calcdialog.CalcDialog;
+import com.example.tugasmobileprog.ui.calculator.CalculatorActivity;
 
 import java.math.BigDecimal;
 
-public class HomeFragment extends Fragment implements CalcDialog.CalcDialogCallback{
-//    private FragmentHomeBinding
-    private BigDecimal value = null;
+public class HomeFragment extends Fragment{
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
 
     }
 
@@ -38,21 +34,13 @@ public class HomeFragment extends Fragment implements CalcDialog.CalcDialogCallb
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        final CalcDialog calcDialog = new CalcDialog();
-
 //         Open dialog button
         Button btn = getView().findViewById(R.id.btn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                calcDialog.getSettings().setInitialValue(value);
-                calcDialog.show(getChildFragmentManager(), "calc_dialog");
+                startActivity(new Intent(getContext(), CalculatorActivity.class));
             }
         });
-    }
-
-    @Override
-    public void onValueEntered(int requestCode, @Nullable BigDecimal value) {
-
     }
 }
